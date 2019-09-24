a30ff6d6b327fefebe9b56d477eb5deeadf22fd7
Cory Rentz <48699510+mrsniffles905@users.noreply.github.com>
2019-09-24 14:47:08 -0400

    Add files via upload

---
+++ MinimapCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float yOffset;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = playerTransform.position;
+        targetPosition.y += yOffset;
+        transform.position = targetPosition;
+        
+    }
+}
