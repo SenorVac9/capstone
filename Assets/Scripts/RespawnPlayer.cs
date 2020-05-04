b7df34f610f76d31c04a213f995d9cc02878e5fd
Phoenixdu <freddy.bedran@hotmail.com>
2020-05-04 13:01:35 -0400

    Added track and pause menu
    niel might be good we will see

---
+++ Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    
+    public Transform PlayerRespawnPoint;
+    public GameObject PlayerPrefab;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Boundaries")
+        {
+            Instantiate(PlayerRespawnPoint, PlayerRespawnPoint.position, PlayerRespawnPoint.rotation);
+        }
+        else if (col.gameObject.tag == "Death")
+        {
+            Instantiate(PlayerPrefab, PlayerRespawnPoint.position, PlayerRespawnPoint.rotation);
+        }
+    }
+
+}
