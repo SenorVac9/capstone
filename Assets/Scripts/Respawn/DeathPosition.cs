c3899d34dd9fbda424cd7a1490c3b4a2872a29db
Peter Tan <purpl3grape@gmail.com>
2020-06-09 22:58:18 -0400

    - Polish Update (vehicle physics / colliders / cam)
    - Colliders Added
    - I've addressed the Loop issue (No Flicker at top)
    - Tweaked Vehicle slope rotation handling (Part 1) - Smoother handling. (ToDo more object layer assignment)
    - Tweaked the cam to stay upward with the Car when going around the loop as it was a bit disorienting.
    
    Added general colliders.

--- Assets/Scripts/Respawn/DeathPosition.cs
+++ Assets/Scripts/Respawn/DeathPosition.cs
@@ -22,9 +22,16 @@ public class DeathPosition : MonoBehaviour
 
             vehicleObj.transform.forward = respawnTransform.forward;
             other.GetComponent<VehicleBehavior>().vehicle_heading_transform.forward = respawnTransform.forward;
+            other.GetComponent<VehicleBehavior>().vehicleSpinOutTransform.forward = respawnTransform.forward;
+            other.GetComponent<VehicleBehavior>().vehicle_model_transform.forward = respawnTransform.forward;
+            other.GetComponent<VehicleBehavior>().vehicle_heading_transform.rotation = Quaternion.Euler(0, other.GetComponent<VehicleBehavior>().vehicle_heading_transform.rotation.y, 0);
+            other.GetComponent<VehicleBehavior>().vehicleSpinOutTransform.rotation = Quaternion.Euler(0, other.GetComponent<VehicleBehavior>().vehicleSpinOutTransform.rotation.y, 0);
+            other.GetComponent<VehicleBehavior>().vehicle_model_transform.rotation = Quaternion.Euler(0, other.GetComponent<VehicleBehavior>().vehicle_model_transform.rotation.y, 0);
+            Debug.DrawRay(vehicleObj.transform.position, respawnTransform.forward * 100, Color.red, 10);
+            Debug.DrawRay(vehicleObj.transform.position, other.GetComponent<VehicleBehavior>().vehicle_heading_transform.forward * 100, Color.blue, 10);
+            //Time.timeScale = 0;
             //Debug.DrawRay(transform.position, transform.position + transform.right + vehicleObj.transform.forward, Color.red, 10);
             //Debug.DrawRay(transform.position, transform.position - transform.right + transform.forward, Color.blue, 10);
-            other.GetComponent<VehicleBehavior>().vehicle_heading_transform.rotation = Quaternion.Euler(0, other.GetComponent<VehicleBehavior>().vehicle_heading_transform.rotation.y, 0);
 
 
 
