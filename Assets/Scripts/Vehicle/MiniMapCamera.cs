8832ebe5c9326721ab5aec595cb40a652331511b
gamerboy404 <37173930+gamerboy404@users.noreply.github.com>
2020-06-17 16:14:57 -0400

    MiniMap Added Fixed Wheel Spawnpoint issue

---
+++ Assets/Scripts/Vehicle/MiniMapCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour
+{
+
+    public GameObject localPlayer;
+    public Camera MinimapCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MinimapCam.transform.position = (new Vector3(localPlayer.transform.position.x, MinimapCam.transform.position.y, localPlayer.transform.position.z));
+    }
+}
