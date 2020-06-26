3a727880abde9dd78eab43302e21e46fdbd3a36a
SenorVac9 <abhishroff1@gmail.com>
2020-06-25 22:47:49 -0400

    Revert "ramp fixes with nitro effects"
    This reverts commit be2c23b716eac0aa623c945d7d5ccc7762f24469.

--- Assets/Scripts/Nitro_effect.cs
+++ Assets/Scripts/Nitro_effect.cs
@@ -2,122 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ModuloKart.CustomVehiclePhysics;
-using ModuloKart.HUD;
+
 
 public class Nitro_effect : MonoBehaviour
 {
-
+   
     VehicleBehavior vehicleBehavior;
-    SimpleUI simpleUI;
-    public ParticleSystem nitro_P;
-    int PlayerID;
+    public ParticleSystem nitro;
     // Start is called before the first frame update
     void Start()
     {
         vehicleBehavior = GameObject.FindObjectOfType<VehicleBehavior>();
-        simpleUI = GameObject.FindObjectOfType<SimpleUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        switch (simpleUI.PlayerID)
+        if (vehicleBehavior.is_nitrosboost==true)
         {
-            case 0:
-                if (Input.GetButton("A_ANYPLAYER"))
+            nitro.Stop();
+            if (!nitro.isEmitting)
+            {
+                nitro.Play();
+                Debug.Log("nitro effect playing N");
+            }
+           
+
+
+        }
+        else if (vehicleBehavior.is_nitrosboost==false)
                 {
-                    nitro_P.Stop();
+            nitro.Stop();
 
-                    if (!nitro_P.isEmitting)
-                    {
-                        // vehicleBehavior.GetComponent<VehicleBehavior>();
-                        nitro_P.Play();
-                        Debug.Log("nitro effect playing N p1");
-                    }
-
-
-
-                }
-                else
-                {
-                    nitro_P.Stop();
-
-
-                }
-                break;
-            case 1:
-                if (Input.GetButtonDown("A_ANYPLAYER"))
-                {
-                    nitro_P.Stop();
-
-                    if (!nitro_P.isEmitting)
-                    {
-                        // vehicleBehavior.GetComponent<VehicleBehavior>();
-                        nitro_P.Play();
-                        Debug.Log("nitro effect playing N p1");
-                    }
-
-
-
-                }
-                else
-                {
-                    nitro_P.Stop();
-
-
-                }
-                break;
-
-            case 2:
-                if (Input.GetButtonDown("A_ANYPLAYER"))
-                {
-                    nitro_P.Stop();
-
-                    if (!nitro_P.isEmitting)
-                    {
-                        // vehicleBehavior.GetComponent<VehicleBehavior>();
-                        nitro_P.Play();
-                        Debug.Log("nitro effect playing N p1");
-                    }
-
-
-
-                }
-                else
-                {
-                    nitro_P.Stop();
-
-
-                }
-                break;
-
-            case 3:
-                if (Input.GetButtonDown("A_ANYPLAYER"))
-                {
-                    nitro_P.Stop();
-
-                    if (!nitro_P.isEmitting)
-                    {
-                        // vehicleBehavior.GetComponent<VehicleBehavior>();
-                        nitro_P.Play();
-                        Debug.Log("nitro effect playing N p1");
-                    }
-
-
-
-                }
-                else
-                {
-                    nitro_P.Stop();
-
-
-                }
-                break;
-
-            default:
-                Debug.Log("Nitro Particle Notworking");
-                break;
         }
     }
 }
